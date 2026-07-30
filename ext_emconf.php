7757f3c6d91305cda04ecd8402fe2e49cfbaf660
Nitin Parri <nitin@refresj.nl>
2026-07-30 13:52:29 +0200

    Raise version to 14.5.0

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Font Awesome for CKEditor',
+    'description' => 'Integrates Font Awesome 7 Pro+ icons into the CKEditor 5 RTE! Font Awesome license required and not included.',
+    'category' => 'be',
+    'author' => 'Nitin Parri',
+    'author_email' => 'nitin@refresj.nl',
+    'author_company' => 'Refresj',
+    'state' => 'stable',
+    'version' => '14.5.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '14.3.0-14.99.99',
+            'php' => '8.5.0-8.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
